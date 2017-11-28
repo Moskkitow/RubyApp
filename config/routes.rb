6dc96eb9a5298f9cecc791b9220a441a4c763e47
Marcelo Aröxa Cruz <marvelo_aroxa@hotmail.com>
2017-11-28 19:44:43 -0300

    Home Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  
+  namespace :site do
+    get 'home', to: 'home#index'
+  end
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
+  
   namespace :backoffice do
     get 'dashboard', to: 'dashboard#index'
   end
 
   devise_for :admins
   devise_for :members
-
-  namespace :site do
-    get 'home', to: 'home#index'
-  end
 
   root 'site/home#index'
 
