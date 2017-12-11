4650abe059fbffca7b099b6919e6bd94212d2515
Marcelo Aröxa Cruz <marvelo_aroxa@hotmail.com>
2017-12-10 22:54:09 -0300

    Major changes

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,18 +1,15 @@
 class Site::Profile::AdsController < Site::ProfileController
-    before_action :set_ad, only: [:edit]
-  
-    def index
-      @ads = Ad.to_the(current_member)
-    end
-  
-    def edit
-      # Se Ad via before_action
-    end
+  before_action :set_ad, only: [:edit]
 
-    private
-
-      def set_ad
-        @ad = Ad.find(params[:id])
-      end
-  
+  def index
+    @ads = Ad.to_the(current_member)
   end
+  def edit
+    
+  end
+
+  private
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
+end
